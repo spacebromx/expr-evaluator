c5c103fc050c67d4a9ad591254f4c34eb3f46943
Alan Christopher Alamillo Medina <realconect@gmail.com>
2020-10-23 16:39:10 -0500

    Refactored app and index content

---
+++ backend/src/app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const app = express()
+
+app.use(morgan('dev'))
+app.use(cors())
+app.use(express.json())
+
+module.exports = app
