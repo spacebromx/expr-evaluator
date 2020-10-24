58f7b158c33e0a50738f4a349fe9e68a98111691
Alan Christopher Alamillo Medina <realconect@gmail.com>
2020-10-24 12:10:44 -0500

    Added create-react-app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Form from './components/Form'
+
+function App() {
+  return (
+    <div className="container m-auto mt-20">
+      <div className="flex mb-4 lg:w-10/12 m-auto">
+        <div className="w-full">
+          <Form />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
